569e0e6e486e85de0aad394c2a87ecd4ce15893f
morpheusthewhite <zffromGerace@hotmail.it>
2019-03-26 22:37:12 +0100

    Upload installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# installing needed packages (Debian)
+sudo apt-get install vim zsh wget
+
+# installing oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# copying dotfiles
+cp -rv --backup=numbered * ~
