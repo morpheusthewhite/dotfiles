2ef775c7611dcd420d837fae057ed3477bfe21e4
morpheusthewhite <zffromGerace@hotmail.it>
2019-03-28 13:50:36 +0100

    Update files

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ sudo apt-get install vim zsh wget zathura
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # copying dotfiles
-cp -rv --backup=numbered * ~
+cp . -rv --backup=numbered * ~
