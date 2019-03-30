6c59849ace312aa5ab78351ca1a36db7749266ef
morpheusthewhite <zffromGerace@hotmail.it>
2019-03-30 19:51:46 +0100

    Add autojump to installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # installing needed packages (Debian)
-sudo apt-get install vim zsh wget zathura
+sudo apt-get install vim zsh wget zathura autojump
 
 # installing oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
