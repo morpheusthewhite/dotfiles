06b65bf51d44aae6088a28464b50e1b7d89c44a7
morpheusthewhite <zffromGerace@hotmail.it>
2020-05-17 11:39:05 +0200

    Update polybar configuration

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar main -c ~/.config/polybar/config.ini &
+polybar main -c ~/.config/polybar/config2.ini &
+#for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    #MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
+#done
