1299b0b655b41eebf7b8d01abed22170ce4fca65
morpheusthewhite <zffromGerace@hotmail.it>
2021-11-20 11:35:53 +0100

    feat(polybar): update config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-## Add this to your wm startup file.
+# Add this script to your wm startup file.
+
 
 # Terminate already running bar instances
 killall -q polybar
@@ -8,9 +9,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar main -c ~/.config/polybar/config.ini &
-polybar main -c ~/.config/polybar/config2.ini &
-#for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    #MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
-#done
+# Launch the bar
+polybar -q main -c ~/.config/polybar/config.ini &
+polybar -q main2 -c ~/.config/polybar/config2.ini &
